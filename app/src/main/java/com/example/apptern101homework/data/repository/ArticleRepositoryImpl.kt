438137971febedfe9d39cff1c7978e8022a9839d
herdal06 <h.erdal721@gmail.com>
2023-07-08 15:15:10 +0300

    Implement room operations in ArticleRepository

--- app/src/main/java/com/example/apptern101homework/data/repository/ArticleRepositoryImpl.kt
+++ app/src/main/java/com/example/apptern101homework/data/repository/ArticleRepositoryImpl.kt
@@ -4,17 +4,21 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import androidx.paging.map
+import com.example.apptern101homework.data.local.dao.ArticleDao
+import com.example.apptern101homework.data.local.entity.toDomain
 import com.example.apptern101homework.data.remote.dto.toDomain
 import com.example.apptern101homework.data.remote.pagingsource.ArticlePagingSource
 import com.example.apptern101homework.data.remote.service.ArticleService
 import com.example.apptern101homework.domain.repository.ArticleRepository
 import com.example.apptern101homework.domain.uimodel.Article
+import com.example.apptern101homework.domain.uimodel.toEntity
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class ArticleRepositoryImpl @Inject constructor(
     private val articleService: ArticleService,
+    private val articleDao: ArticleDao
 ) : ArticleRepository {
     override fun searchNews(searchQuery: String): Flow<PagingData<Article>> {
         return Pager(
@@ -30,6 +34,20 @@ class ArticleRepositoryImpl @Inject constructor(
             pagingData.map {
                 it.toDomain()
             }
+        }
+    }
+
+    override suspend fun insert(article: Article) {
+        articleDao.insert(article.toEntity())
+    }
+
+    override suspend fun delete(article: Article) {
+        articleDao.insert(article.toEntity())
+    }
+
+    override fun getAll(): Flow<List<Article>> {
+        return articleDao.getAll().map { articles ->
+            articles.map { it.toDomain() }
         }
     }
 }
--- app/src/main/java/com/example/apptern101homework/domain/repository/ArticleRepository.kt
+++ app/src/main/java/com/example/apptern101homework/domain/repository/ArticleRepository.kt
@@ -6,4 +6,7 @@ import kotlinx.coroutines.flow.Flow
 
 interface ArticleRepository {
     fun searchNews(searchQuery: String): Flow<PagingData<Article>>
+    suspend fun insert(article: Article)
+    suspend fun delete(article: Article)
+    fun getAll(): Flow<List<Article>>
 }
